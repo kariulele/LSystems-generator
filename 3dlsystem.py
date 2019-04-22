4a84d6e6a71e578a17619e1c99e97f164482b2c7
Danae Marmai <danae.marmai@epita.fr>
2019-04-22 23:57:22 +0200

    almost working cube

--- 3dlsystem.py
+++ 3dlsystem.py
@@ -7,6 +7,23 @@ Created on Mon Apr 22 19:30:37 2019
 """
 import numpy as np
 import math
+import bpy
+
+def draw_cylinder(fr, to, r):
+    dx = to[0] - fr[0]
+    dy = to[1] - fr[1]
+    dz = to[2] - fr[2]    
+    dist = math.sqrt(dx**2 + dy**2 + dz**2)
+    bpy.ops.mesh.primitive_cylinder_add(
+      radius = r, 
+      depth = dist,
+      location = (dx/2 + fr[0], dy/2 + fr[1], dz/2 + fr[2])   
+    ) 
+
+    phi = math.atan2(dy, dx) 
+    theta = math.acos(dz/dist) 
+    bpy.context.object.rotation_euler[1] = theta 
+    bpy.context.object.rotation_euler[2] = phi
 
 def RU(angle):
     return [[math.cos(angle), math.sin(angle), 0],[-math.sin(angle), math.cos(angle), 0],[0, 0, 1]]
@@ -34,7 +51,7 @@ pattern initialisation
 '''
 angled = 90
 pattern = "A"
-iteration = 2
+iteration = 1
 remplacement = {"A": "B-F+CFC+F-D&F∧D-F+&&CFC+F+B//", "B": "A&F∧CFB∧F∧D∧∧-F-D∧|F∧B|FC∧F∧A//", "C":"|D∧|F∧B-F+C∧F∧A&&FA&F∧C+F+B∧F∧D//", "D":"|CFB-F+B|FA&F∧A&&FB-F+B|FC//"}
 
 '''
@@ -45,7 +62,7 @@ res = derivation(pattern, remplacement, iteration)
 
 def draw(pattern, angled):
     loc = [0, 0, 0]
-    diameter = 1
+    diameter = 0.1
     colorIndex = 0
     H = [1, 0, 0]
     L = [0, 1, 0]
@@ -62,10 +79,10 @@ def draw(pattern, angled):
     stack = []
     for i in pattern:
         if (i == "F"):
-            '''
-            draw stuff
-            '''
             D = M[0]
+            newloc = np.add(loc, D)
+            draw_cylinder(loc, newloc, diameter)
+            loc = newloc
         elif (i == "+"):
             M = np.dot(M, ru)
         elif (i == "-"):
@@ -88,6 +105,7 @@ def draw(pattern, angled):
             stack.append((M, loc))
         elif (i == "]"):
             M, loc = stack.pop()
-        
+
+draw(res, 90)
 
 
