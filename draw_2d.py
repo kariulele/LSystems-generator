168ce072b78a504621c4dac110514ab2b6e77526
Danae Marmai <danae.marmai@epita.fr>
2019-04-25 22:05:18 +0200

    add a function to draw in 2d with turtle with a color parameter

---
+++ draw_2d.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import turtle as tl
+
+def draw_turtle(tree, angle, color):
+    stack = []
+    tl.up()
+    tl.left(90)
+    tl.setpos(0, -350)
+    tl.down()
+    tl.pensize(2)
+    j = -1
+    for i in tree:
+        j += 1
+        if (i == "F"):
+            tl.forward(8);
+        elif (i == "+"):
+            tl.left(angle)
+        elif (i == "-"):
+            tl.right(angle)
+        elif(i == "["):
+            stack.append((tl.position(), tl.heading()))
+        elif (i == "]"):
+            tl.up()
+            ((x,y), t) = stack.pop()
+            tl.setheading(t)
+            tl.setpos(x, y)
+            tl.down()
+        elif (i == "C"):
+            tl.pencolor(color[int(tree[j+1])])
+    tl.up()
+    tl.setpos(0,-350)
+    tl.done()
