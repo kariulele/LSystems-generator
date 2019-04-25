618fc5d5293b18e154f4ac8e5a8cbe7ae56a393e
Danae Marmai <danae.marmai@epita.fr>
2019-04-25 22:04:03 +0200

    add a stochatic derivation function

--- derivation.py
+++ derivation.py
@@ -6,6 +6,8 @@ Created on Mon Apr 22 19:30:37 2019
 @author: heldhy
 """
 
+import random
+
 def derivation(pattern, rempl, iteration):
     res = pattern
     for k in range(iteration):
@@ -14,6 +16,25 @@ def derivation(pattern, rempl, iteration):
         for i in pattern:
             if (i in rempl):
                 res += rempl[i]
+            else:
+                res += i
+    return res
+
+def stock_derivation(pattern, rempl, iteration):
+    r = random.Random()
+    res = pattern
+    for k in range(iteration):
+        pattern = res
+        res = ""
+        for i in pattern:
+            if (i in rempl):
+                proba = r.random()
+                l = list(rempl[i].keys())
+                l.sort()
+                for u in l:
+                    if (proba <= u):
+                        res += rempl[i][u]
+                        break
             else:
                 res += i
     return res
