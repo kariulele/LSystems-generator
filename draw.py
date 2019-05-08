65ec235e45eac5e629e50efe501f068394182f8a
Danae Marmai <danae.marmai@epita.fr>
2019-05-08 23:10:21 +0200

    add apple

--- draw.py
+++ draw.py
@@ -9,6 +9,62 @@ import numpy as np
 import math
 import bpy
 from matrix_rotation import RH, RU, RL
+from draw3dleaf import *
+
+def draw_straight(pattern, angled, color = 0):
+    if (not("BRN" in bpy.data.materials)):
+        mat = bpy.data.materials.new("BRN")
+        mat.diffuse_color = (0.6,0.44,0.16)
+    else:
+        mat = bpy.data.materials["BRN"]
+    loc = [0, 0, 0]
+    diameter = 0.1
+    colorIndex = 0
+    H = [0, 0, 1]
+    L = [1, 0, 0]
+    U = [0, 1, 0]
+    M = [H, L, U]
+    angle = math.radians(angled)
+    rh = RU(angle)
+    rl = RH(angle)
+    ru = RL(angle)
+    rhn = RU(-angle)
+    rln = RH(-angle)
+    run = RL(-angle)
+    ru180 = RL(math.radians(180))
+    stack = []
+    for i in pattern:
+        if (i == "F"):
+            D = M[0]
+            newloc = np.add(loc, D)
+            draw_cylinder(loc, newloc, diameter, mat)
+            loc = newloc
+        elif (i == "+"):
+            M = np.dot(M, ru)
+        elif (i == "-"):
+            M = np.dot(M, run)
+        elif (i == "<"):
+            M = np.dot(M, rhn)
+        elif (i == ">"):
+            M = np.dot(M, rh)
+        elif (i == "&"):
+            M = np.dot(M, rl)
+        elif (i == "^"):
+            M = np.dot(M, rln)
+        elif (i == "|"):
+            M = np.dot(M, ru180)
+        elif (i == "!"):
+            diameter -= 0.01
+        elif (i == "'"):
+            colorIndex += 1
+        elif (i == "["):
+            stack.append((M, loc, diameter))
+        elif (i == "]"):
+            M, loc, diameter = stack.pop()
+        elif (i == "L"):
+            draw_leaf(loc)
+        elif (i == "f"):
+            draw_flower(loc, color)
 
 def draw_cylinder(fr, to, r, color):
     dx = to[0] - fr[0]
